c6186f12d882bd43185cd48ab5f1de31ad46c50f
david686 <dbbelhamoudavid@gmail.com>
2023-10-02 12:26:34 +0300

    change status

--- RTK/main.c
+++ RTK/main.c
@@ -4,37 +4,36 @@
 #include<time.h>
 
 int main() {
-	//while (1)
-	//{
-	//	
-	//	char input[50];
-	//	printf("Please enter one of the following command: \n");
-	//	printf("1. quit\n2.(LOG_OUTPUT_POLICY_NONE or LOG_OUTPUT_POLICY_FILE or LOG_OUTPUT_POLICY_STDOUT)");
-	//	if (strcmp(input, 'quit') == 0) {
-	//		exit(0);
-	//	}
-	//	else if (strcmp(input, "LOG_OUTPUT_POLICY_NONE")) {
-	//		// do nothing the policy is already in None Mode
-	//		break;
-	//	}
-	//	else if (strcmp(input, "LOG_OUTPUT_POLICY_FILE")) {
-	//		logger_init(LOG_OUTPUT_POLICY_FILE);
-	//		break;
-	//	}
-	//	else if (strcmp(input, "LOG_OUTPUT_POLICY_STDOUT")) {
-	//		logger_init(LOG_OUTPUT_POLICY_STDOUT);
-	//		break;
-	//	}
-	//	else if ((strcmp(input, "LOG_OUTPUT_POLICY_STDOUT | LOG_OUTPUT_POLICY_FILE")) || (strcmp(input ,"LOG_OUTPUT_POLICY_FILE | LOG_OUTPUT_POLICY_STDOUT"))){
-	//		logger_init(LOG_OUTPUT_POLICY_FILE || LOG_OUTPUT_POLICY_STDOUT);
-	//		break;
-	//	}
-	//	else
-	//	{
-	//		printf("You did not enter a valid inut");
-	//		continue;
-	//	}
-	//
-	//}
-	now();
+	while (1)
+	{
+		
+		char input[50];
+		printf("Please enter one of the following command: \n");
+		printf("1. quit\n2.(LOG_OUTPUT_POLICY_NONE or LOG_OUTPUT_POLICY_FILE or LOG_OUTPUT_POLICY_STDOUT)");
+		if (strcmp(input, 'quit') == 0) {
+			exit(0);
+		}
+		else if (strcmp(input, "LOG_OUTPUT_POLICY_NONE")) {
+			// do nothing the policy is already in None Mode
+			break;
+		}
+		else if (strcmp(input, "LOG_OUTPUT_POLICY_FILE")) {
+			logger_init(LOG_OUTPUT_POLICY_FILE);
+			break;
+		}
+		else if (strcmp(input, "LOG_OUTPUT_POLICY_STDOUT")) {
+			logger_init(LOG_OUTPUT_POLICY_STDOUT);
+			break;
+		}
+		else if ((strcmp(input, "LOG_OUTPUT_POLICY_STDOUT | LOG_OUTPUT_POLICY_FILE")) || (strcmp(input ,"LOG_OUTPUT_POLICY_FILE | LOG_OUTPUT_POLICY_STDOUT"))){
+			logger_init(LOG_OUTPUT_POLICY_FILE || LOG_OUTPUT_POLICY_STDOUT);
+			break;
+		}
+		else
+		{
+			printf("You did not enter a valid input");
+			continue;
+		}
+	
+	}
 }
